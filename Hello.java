d12b7280981ca5fef6ef1b99dd2d0ae6a7c00e17
SilentHub <sanek.02200119@gmail.com>
2021-09-15 21:55:33 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,6 @@ public class Hello {
 
     public static void main(String[] args) {
 	System.out.println("A");
-        System.out.println("Hello world!1");
+        System.out.println("Hello world!");
     }
 }
