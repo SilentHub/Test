34e4f541cbb1819469fef888d1411d145f5c5e29
Alexander <sanek.02200119@gmail.com>
2021-09-15 22:07:49 +0300

    B

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ package com.silent27;
 public class Hello {
 
     public static void main(String[] args) {
-	System.out.println("A");
         System.out.println("Hello world");
+	System.out.println("B");
     }
 }
